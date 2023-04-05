379bb56a2d5e3468dbcb623f3a382e10e289aa07
towficaziz <tapolly01@gmail.com>
2023-04-05 02:36:17 +0200

    Initiating the Practice service assignment and components

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ActiveUsersComponent } from './active-users/active-users.component';
+import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ActiveUsersComponent,
+    InactiveUsersComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
